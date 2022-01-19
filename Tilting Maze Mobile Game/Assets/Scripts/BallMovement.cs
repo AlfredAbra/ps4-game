c442b89ab7af8801777acc22b6082dc0fa2baeac
alfred <k1914899@kingston.ac.uk>
2022-01-19 20:48:42 +0000

    vibration, controls and collaberate

--- Tilting Maze Mobile Game/Assets/Scripts/BallMovement.cs
+++ Tilting Maze Mobile Game/Assets/Scripts/BallMovement.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.PS4;
@@ -20,7 +21,6 @@ public class BallMovement : MonoBehaviour
 
     public float sensH = 10;
     public float sensV = 10;
-    public float speed;
 
     public int playerId;
 
@@ -36,39 +36,47 @@ public class BallMovement : MonoBehaviour
     {
         if (PS4Input.PadIsConnected(playerId)) 
         {
+            // Option Key...
+            KeyCode code = (KeyCode)Enum.Parse(typeof(KeyCode), "Joystick1Button7", true);
+            if (Input.GetKey(code))
+                PS4Input.PadResetOrientation(playerId);
+
             //Movement
             Vector4 v = PS4Input.PadGetLastOrientation(0);
-
-            float moveHor = Mathf.Clamp(v.z * sensH, -1, 1);
-            float moveVer = Mathf.Clamp(v.x * sensV, -1, 1);
-            rb.AddForce(new Vector3(-moveHor, 0, -moveVer) * speed * Time.deltaTime);
-        }
             // The acceleration.y variable is used within the z-axis part of the Vector3 variable so it matches the phone's tilt correctly (so if the phone is tilted forward it moves on te z-axis in the game scene).
-            //Vector3 ballTilt = new Vector3 (Input.acceleration.x, 0f, Input.acceleration.y);
+            Vector3 ballTilt = new Vector3(-v.z, 0f, v.x);
 
-        // This adds the velocity to the ball game object and also uses the ballSpeed value as well to move at a certain speed.
-        //rb.velocity = ballTilt * ballSpeed;
+            // This adds the velocity to the ball game object and also uses the ballSpeed value as well to move at a certain speed.
+            rb.velocity = ballTilt * ballSpeed;
 
-        // This moves the ball back to original start position if the player falls off the narrow bridge sections.
-        if(ball.transform.position.y < -0.31f)
-        {
-            soundEffects.PlayOneShot(ballExplosionSound, 0.75f);
+            // This moves the ball back to original start position if the player falls off the narrow bridge sections.
+            if (ball.transform.position.y < -0.31f)
+            {
+                soundEffects.PlayOneShot(ballExplosionSound, 0.75f);
 
-            ball.transform.position = new Vector3(0.418107271f, 1, 0.0181865692f);
+                ball.transform.position = new Vector3(0.418107271f, 1, 0.0181865692f);
+            }
         }
+
+    }
+
+    private void FixedUpdate()
+    {
+        
     }
 
     void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.tag == "Ice")
         {
+            Debug.Log("Collided With Ice");
             ballSpeed = 40f;
         }
 
         if (collision.gameObject.tag == "Spikes")
         {
             soundEffects.PlayOneShot(spikeImpactSound,0.75f);
-
+            PS4Input.PadSetVibration(playerId, 255, 255);
             ball.transform.position = new Vector3(0.418107271f, 1f, 0.0181865692f);
         }
 
